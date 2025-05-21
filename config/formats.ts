46797e326a33a0ee042cc35da50f9ad207ea2c4b
Shadrake1 <130673769+Shadrake1@users.noreply.github.com>
2025-05-21 19:43:33 -0400

    fixed formats maybe

--- config/formats.ts
+++ config/formats.ts
@@ -31,15 +31,7 @@ export const Formats: FormatList = [
 		desc: 'bare bones micrometa',
 		mod: 'vtubers',
 		ruleset: [
-		'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Terastal Clause', 'Data Mod', 'Camomons Mod',
-		],
-		banlist: ['All Items', 'All Abilities'],
-		unbanlist: [
-		'Pinch Berry', 'Tsersi Berry', 'Leftovers', 'Silk Scarf', 'Charcoal', 'Mystic Water', 'Miracle Seed', 'Magnet', 'Never-Melt Ice', 'Black Belt', 'Poison Barb',
-		'Soft Sand', 'Sharp Beak', 'Twisted Spoon', 'Silver Powder', 'Hard Stone', 'Spell Tag', 'Dragon Fang', 'Black Glasses', 'Metal Coat', 'Fairy Feather', 'Muscle Band',
-		'Wise Glasses', 'Exchanger', 'Focus Sash',
-		
-		'Desperation', 'Last Stand', 'Appraisal', 'Rejuvenate', 'Recycler', 'Somewhat Reckless', 'Tinted Tactics', 'Intimidate', 'Sceptic',
+		'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Terastal Clause', 'Data Mod',
 		],
 		onValidateTeam(team, format) {
 			/**@type {{[k: string]: true}}*/
@@ -48,7 +40,7 @@ export const Formats: FormatList = [
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
-					return [set.species + ' is not in Bare Bones.'];
+					return [set.species + ' is not in Vtubers.'];
 				}
 			}
 		}
